75d8dd181008d6766539c7edfe24ff9451101dd6
grillbiff <erik.gustaf.johansson@gmail.com>
2020-04-12 15:33:53 +0200

    fixing tcp connection

--- lib/src/connection.dart
+++ lib/src/connection.dart
@@ -38,15 +38,18 @@ class PostgreSQLConnection extends Object
   /// [queryTimeoutInSeconds] refers to the default timeout for [PostgreSQLExecutionContext]'s execute and query methods.
   /// [timeZone] is the timezone the connection is in. Defaults to 'UTC'.
   /// [useSSL] when true, uses a secure socket when connecting to a PostgreSQL database.
-  PostgreSQLConnection(this.host, this.port, this.databaseName,
-      {this.username,
-      this.password,
-      this.timeoutInSeconds = 30,
-      this.queryTimeoutInSeconds = 30,
-      this.timeZone = 'UTC',
-      this.useSSL = false,
-      this.isUnixSocket = false,
-      }) {
+  PostgreSQLConnection(
+    this.host,
+    this.port,
+    this.databaseName, {
+    this.username,
+    this.password,
+    this.timeoutInSeconds = 30,
+    this.queryTimeoutInSeconds = 30,
+    this.timeZone = 'UTC',
+    this.useSSL = false,
+    this.isUnixSocket = false,
+  }) {
     _connectionState = _PostgreSQLConnectionStateClosed();
     _connectionState.connection = this;
   }
@@ -84,7 +87,7 @@ class PostgreSQLConnection extends Object
   /// The processID of this backend.
   int get processID => _processID;
 
-  /// If true, connection is made via unix socket. 
+  /// If true, connection is made via unix socket.
   final bool isUnixSocket;
 
   /// Stream of notification from the database.
@@ -142,15 +145,14 @@ class PostgreSQLConnection extends Object
 
     try {
       _hasConnectedPreviously = true;
-      InternetAddress addr;
       if (isUnixSocket) {
-        addr = InternetAddress(host, type: InternetAddressType.unix);
+        _socket = await Socket.connect(
+                InternetAddress(host, type: InternetAddressType.unix), port)
+            .timeout(Duration(seconds: timeoutInSeconds));
       } else {
-        addr = InternetAddress(host);
+        _socket = await Socket.connect(host, port)
+            .timeout(Duration(seconds: timeoutInSeconds));
       }
-
-      _socket = await Socket.connect(addr, port)
-          .timeout(Duration(seconds: timeoutInSeconds));
 
       _framer = MessageFramer();
       if (useSSL) {
